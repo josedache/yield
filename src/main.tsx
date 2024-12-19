d9855d9bf03dffb6a0a6c59006c33cea50ca4291
Adun <adun@users-MacBook-Pro.local>
2024-12-19 17:11:16 +0100

    Integrating Sentry

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,21 @@ import MuiThemeProvider from "providers/MuiThemeProvider";
 import MuiDatePickerProvider from "providers/MuiDatePickerProvider";
 import MuiSnackbarProvider from "providers/MuiSnackbarProvider";
 import ReduxStoreProvider from "providers/ReduxStoreProvider";
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://94b24310bc649060f4f2ece29182e797@o4504095760711680.ingest.sentry.io/4506076965699584",
+  // Alternatively, use `process.env.npm_package_version` for a dynamic release version
+  release: "my-project-name@2.3.12",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost",  /^https:\/\/40\.113\.169\.208:9010\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
