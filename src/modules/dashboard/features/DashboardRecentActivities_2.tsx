2f521236cc799db9aa3e6c2fb9c473794bc05c9d
Joseph Edache <josedache0@gmail.com>
2025-04-03 22:48:35 +0100

    feat: dashboard recent activities

--- src/modules/dashboard/features/DashboardRecentActivities_2.tsx
+++ src/modules/dashboard/features/DashboardRecentActivities_2.tsx
@@ -13,7 +13,7 @@ import CurrencyTypography from "components/CurrencyTypography.tsx";
 import * as dfns from "date-fns";
 import { TransactionType } from "enums/TransactionType.ts";
 
-function DashboardRecentActivities() {
+function DashboardRecentActivities_2() {
   const transactionsParentRef = useRef(null);
 
   const [infiniteTransactions, setInfiniteTransactions] = useState<{
@@ -39,10 +39,32 @@ function DashboardRecentActivities() {
     ),
   );
 
-  const transactions = (() => {
-    return Object.values(infiniteTransactions).flatMap(
+  const transactionSections = (() => {
+    const transactions = Object.values(infiniteTransactions).flatMap(
       (transactions) => transactions,
     );
+
+    const groupedTransactions = transactions?.reduce(
+      (acc, curr) => {
+        if (acc[curr.transaction_date]) {
+          acc[curr.transaction_date].transactions.push(curr);
+        } else {
+          acc[curr.transaction_date] = {
+            date: curr.transaction_date,
+            transactions: [curr],
+          };
+        }
+
+        return acc;
+      },
+      {} as { [x: string]: TransactionSection },
+    );
+
+    if (!groupedTransactions) {
+      return [];
+    }
+
+    return Object.values(groupedTransactions);
   })();
 
   const topItemIntersectionObserver = useIntersectionObserver((entries) => {
@@ -73,7 +95,7 @@ function DashboardRecentActivities() {
   }, []);
 
   const virtualizer = useVirtualizer({
-    count: transactions?.length,
+    count: transactionSections?.length,
     getScrollElement: () => transactionsParentRef.current,
     estimateSize: () => 100,
     overscan: 5,
@@ -82,13 +104,11 @@ function DashboardRecentActivities() {
   const virtualItems = virtualizer.getVirtualItems();
 
   useEffect(() => {
-    if (transactionsQueryResult.originalArgs) {
-      setInfiniteTransactions((p) => ({
-        ...p,
-        [JSON.stringify(transactionsQueryResult.originalArgs)]:
-          transactionsQueryResult.data?.data ?? [],
-      }));
-    }
+    setInfiniteTransactions((p) => ({
+      ...p,
+      [JSON.stringify(transactionsQueryResult.originalArgs)]:
+        transactionsQueryResult.data?.data ?? [],
+    }));
   }, [
     transactionsQueryResult.originalArgs,
     transactionsQueryResult.data?.data,
@@ -117,7 +137,7 @@ function DashboardRecentActivities() {
         >
           {() => (
             <>
-              {transactions?.length ? (
+              {transactionSections?.length ? (
                 <div
                   className="overflow-y-auto h-96 px-4 md:px-8"
                   style={{ contain: "strict" }}
@@ -130,7 +150,7 @@ function DashboardRecentActivities() {
                     }}
                   >
                     <div
-                      className="absolute left-0 top-0 w-full"
+                      className="absolute left-0 top-0 w-full space-y-8"
                       style={{
                         transform: `translateY(${
                           virtualItems[0]?.start ?? 0
@@ -138,16 +158,17 @@ function DashboardRecentActivities() {
                       }}
                     >
                       {virtualItems.map((virtualItem) => {
-                        const transaction = transactions?.[virtualItem.index];
+                        const transactionSection =
+                          transactionSections?.[virtualItem.index];
                         return (
                           <div
-                            key={transaction?.id}
+                            key={transactionSection?.date}
                             data-index={virtualItem.index}
                             ref={virtualizer.measureElement}
                           >
-                            <DashboardRecentActivityTransaction
-                              key={transaction?.id}
-                              transaction={transaction}
+                            <DashboardRecentActivitySection
+                              key={transactionSection?.date}
+                              section={transactionSection}
                             />
                           </div>
                         );
@@ -185,7 +206,36 @@ function DashboardRecentActivities() {
   );
 }
 
-export default DashboardRecentActivities;
+export default DashboardRecentActivities_2;
+
+function DashboardRecentActivitySection(props: {
+  section: TransactionSection;
+}) {
+  const { section } = props;
+  const date = new Date(section.date);
+  const yearStart = dfns.startOfYear(new Date());
+
+  return (
+    <>
+      <div className="space-y-2">
+        <Typography variant="h6" className="font-bold">
+          {dfns.isBefore(date, yearStart)
+            ? dfns.format(date, "dd MMMM yyyy")
+            : dfns.format(date, "dd MMMM")}
+        </Typography>
+
+        <div className="space-y-4">
+          {section.transactions?.map((transaction) => (
+            <DashboardRecentActivityTransaction
+              key={transaction?.id}
+              transaction={transaction}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
 
 function DashboardRecentActivityTransaction(props: {
   transaction: Transaction;
@@ -198,7 +248,7 @@ function DashboardRecentActivityTransaction(props: {
 
   return (
     <>
-      <div className="flex items-center gap-4 py-2">
+      <Paper className="p-4 flex items-center gap-4">
         <IconButton
           variant="soft"
           color={
@@ -220,29 +270,24 @@ function DashboardRecentActivityTransaction(props: {
           <Iconify
             icon={
               {
-                // [TransactionType.withdrawal]: "uis:arrow-up-right",
-                [TransactionType.withdrawal]: "ic:baseline-minus",
-                // [TransactionType.deposit]: "uis:arrow-down-left",
-                [TransactionType.deposit]: "ic:twotone-plus",
+                [TransactionType.withdrawal]: "uis:arrow-up-right",
+                [TransactionType.deposit]: "uis:arrow-down-left",
                 [TransactionType.card]: "famicons:card-outline",
                 [TransactionType.airtime]: "fluent:phone-24-regular",
                 [TransactionType.cable]: "streamline:satellite-dish",
                 [TransactionType.electricity]: "mage:electricity",
-                // [TransactionType.overdraft]: "uis:arrow-down-left",
-                [TransactionType.overdraft]: "ic:twotone-plus",
-                // [TransactionType.feeDeduction]: "uis:arrow-up-right",
-                [TransactionType.feeDeduction]: "ic:baseline-minus",
+                [TransactionType.overdraft]: "uis:arrow-down-left",
+                [TransactionType.feeDeduction]: "uis:arrow-up-right",
                 [TransactionType.yield]: "uis:arrow-up-right",
-                // [TransactionType.interest]: "uis:arrow-down-left",
-                [TransactionType.interest]: "ic:sharp-percent",
-                // [TransactionType.other]: "famicons:card-outline",
-                [TransactionType.other]: "ic:sharp-percent",
+                [TransactionType.interest]: "uis:arrow-down-left",
+                [TransactionType.other]: "famicons:card-outline",
               }[transaction?.transaction_type] as any
             }
           />
         </IconButton>
-        <div>
-          <Typography variant="body1" className="space-x-1" gutterBottom>
+
+        <div className="flex-1 space-y-0.5">
+          <Typography className="space-x-1">
             {transaction?.mobile_label
               ?.split(" ")
               .filter((w) => !!w)
@@ -260,25 +305,32 @@ function DashboardRecentActivityTransaction(props: {
                     {newWord}
                   </span>
                 );
-              }) || "----"}
+              })}
           </Typography>
-          <Typography variant="body2" color="textSecondary">
-            {dfns.format(
-              new Date(transaction?.transaction_date),
-              "dd MMM, yyyy",
-            )}
-          </Typography>
+          <Typography>{transaction?.transaction_category}</Typography>
         </div>
-        <div className="flex-1" />
-        <Typography
-          className={clsx(isDebit ? "text-error-main" : "text-success-main")}
-        >
-          {isDebit ? "-" : "+"}
-          <CurrencyTypography component="span">
-            {transaction?.amount}
-          </CurrencyTypography>
-        </Typography>
-      </div>
+
+        <div className="text-right">
+          <Typography
+            className={clsx(isDebit ? "text-error-main" : "text-success-main")}
+          >
+            {isDebit ? "-" : "+"}
+            <CurrencyTypography component="span">
+              {transaction?.amount}
+            </CurrencyTypography>
+          </Typography>
+          {transaction?.transaction_time ? (
+            <Typography variant="body2">
+              {dfns.format(
+                new Date(transaction?.transaction_time),
+                "hh:mm aaa",
+              )}
+            </Typography>
+          ) : null}
+        </div>
+      </Paper>
     </>
   );
 }
+
+type TransactionSection = { date: string; transactions: Transaction[] };
